7372f066772c8f8fd137b09f70eb82b2afe9a099
Akhilesh Nevatia <akhilesh.nevatia@gmail.com>
2023-12-06 00:07:42 -0600

    Adding header file implementation and including it in the cpp file

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -1,11 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
+// OrderBook.cpp
+
+#include "OrderBook.hpp"
 #include <iostream>
-#include <map>
-#include <string>
 
-using namespace std;
+std::string TICKER = "GOOGL"; // Say we are making this ticker to trade for a dummy google stock
 
-map<int,int> bid,ask; // map of price to quantity for bid and ask arrays in orderbook
+void OrderBook::flipBalance(const std::string& userId1, const std::string& userId2, double quantity, double price) {
+    // Implementation of flipBalance
+    // This function is not defined in the provided header file, so you need to provide its implementation based on your requirements.
+}
 
+double OrderBook::fillOrders(const std::string& side, double price, double quantity, const std::string& userId) {
+    // Implementation of fillOrders
+    // This function is not defined in the provided header file, so you need to provide its implementation based on your requirements.
+    return 0.0; // Placeholder, adjust return type as needed
+}
 
+OrderBook::OrderBook() {
+    // Initialize your order book as needed
+    
+}
+
+OrderBook::~OrderBook() {
+    // Cleanup tasks go here
+    // For example, deallocate any dynamically allocated memory or close file handles
+}
+
+std::string OrderBook::add_bid(Order bid, User user) {
+    // Implementation of add_bid
+    // This function is not defined in the provided header file, so you need to provide its implementation based on your requirements.
+    return "Bid added successfully."; // Placeholder, adjust return type as needed
+}
+
+std::string OrderBook::add_ask(Order ask, User user) {
+    // Implementation of add_ask
+    // This function is not defined in the provided header file, so you need to provide its implementation based on your requirements.
+    return "Ask added successfully."; // Placeholder, adjust return type as needed
+}
+
+std::string OrderBook::getBalance(User username) {
+    // Implementation of getBalance
+    // This function is not defined in the provided header file, so you need to provide its implementation based on your requirements.
+    return "Balance retrieved successfully."; // Placeholder, adjust return type as needed
+}
+
+std::string OrderBook::getQuote() {
+    // Implementation of getQuote
+    // This function is not defined in the provided header file, so you need to provide its implementation based on your requirements.
+    return "Quote retrieved successfully."; // Placeholder, adjust return type as needed
+}
+
+std::string OrderBook::getDepth() {
+    // Implementation of getDepth
+    // This function is not defined in the provided header file, so you need to provide its implementation based on your requirements.
+    return "Depth retrieved successfully."; // Placeholder, adjust return type as needed
+}
--- src/orderbook.hpp
+++ src/orderbook.hpp
@@ -2,9 +2,9 @@
 
 #ifndef ORDERBOOK_HPP
 #define ORDERBOOK_HPP
-
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 
 std :: string TICKER = "GOOGL"; // ticker for the stock we are trading
@@ -29,19 +29,25 @@ struct Order{
 
 
 
-
 class OrderBook{
+private:
 
-
+    std :: vector<Order> bids;
+    std :: vector<Order> asks;
+    std :: unordered_map <std :: string,User> users; // stores unique user name and balances for each user
+    void flipBalance(const std::string& userId1, const std::string& userId2, double quantity, double price);
+    double fillOrders(const std::string& side, double price, double quantity, const std::string& userId);
 
     public:
+    OrderBook(); // constructor
+   ~OrderBook();
+    std:: string add_bid(Order bid, User user); // adds a bid or ask to the order book
+    std:: string add_ask(Order ask, User user); // adds a bid or ask to the order book
+    std:: string getBalance(User username); // returns the balance of a user
+    std:: string getQuote(); // returns the best bid and ask prices and quantities
+    std:: string getDepth(); // returns the entire order book and shows all bids and asks
 
-}
-
-
-
-
-
+};
 
 
 #endif
